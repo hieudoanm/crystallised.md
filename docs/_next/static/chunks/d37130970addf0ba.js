00cfedfe226a437eced2a04dec0a3e5c0de48245
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-10-27 23:55:37 +0700

    remove note

--- docs/_next/static/chunks/d37130970addf0ba.js
+++ docs/_next/static/chunks/d37130970addf0ba.js
@@ -2,7 +2,7 @@ __turbopack_load_page_chunks__('/_app', [
 	'static/chunks/0db5bb38c94ace5b.js',
 	'static/chunks/a95e57cabc9e4573.js',
 	'static/chunks/ecce6fe44205d99d.js',
-	'static/chunks/07caef24eca7b0aa.css',
+	'static/chunks/8203429701e49074.css',
 	'static/chunks/60574c59fd8f3dd6.css',
-	'static/chunks/turbopack-fc07816ee9fced49.js',
+	'static/chunks/turbopack-940b514e26357f19.js',
 ]);
