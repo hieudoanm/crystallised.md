9856f0cc0546bf20a1225a43d72f3b5e83653852
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-10-28 08:40:51 +0700

    remove note

--- docs/_next/static/chunks/98fe6465b4a4df56.js
+++ docs/_next/static/chunks/98fe6465b4a4df56.js
@@ -2,7 +2,7 @@ __turbopack_load_page_chunks__('/_app', [
 	'static/chunks/0db5bb38c94ace5b.js',
 	'static/chunks/a95e57cabc9e4573.js',
 	'static/chunks/ecce6fe44205d99d.js',
-	'static/chunks/8203429701e49074.css',
+	'static/chunks/cec8700ff7cc4aee.css',
 	'static/chunks/60574c59fd8f3dd6.css',
-	'static/chunks/turbopack-940b514e26357f19.js',
+	'static/chunks/turbopack-5092e9cd8b470b21.js',
 ]);
--- docs/_next/static/chunks/turbopack-5092e9cd8b470b21.js
+++ docs/_next/static/chunks/turbopack-5092e9cd8b470b21.js
@@ -5,7 +5,7 @@
 			'static/chunks/0db5bb38c94ace5b.js',
 			'static/chunks/a95e57cabc9e4573.js',
 			'static/chunks/ecce6fe44205d99d.js',
-			'static/chunks/8203429701e49074.css',
+			'static/chunks/cec8700ff7cc4aee.css',
 			'static/chunks/60574c59fd8f3dd6.css',
 		],
 		runtimeModuleIds: [8522, 4757],
